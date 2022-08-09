90872776e0c2ffb627ddd7b5a44a4b880974ec6d
Yash15o2 <yashthakur15022002@gmail.com>
2022-08-09 16:15:09 +0530

    Initial version 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import moon from './img/moon.svg';
+import plus from './img/plus.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [num1, setNum1] = useState(0);
+	const [num2, setNum2] = useState(0);
+	const [text, setText] = useState('Answer');
+
+	function randomNumberInRange(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
+	const handleClick = () => {
+		setNum1(randomNumberInRange(1, 9));
+		setNum2(randomNumberInRange(1, 9));
+		setText('Answer');
+	};
+	const handleText = () => {
+		setText(num1 + num2);
+	};
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<div></div>
+				<div className="logo">Add.me</div>
+				<div>
+					<img className="moon-img" src={moon} alt="Dark Mode" />
+				</div>
+			</header>
+
+			<section className="App-section">
+				<div className="sec-div">
+					<div className="number">{num1}</div>
+					<div>
+						<img className="plus-img" src={plus} alt="Plus" />
+					</div>
+					<div className="number">{num2}</div>
+				</div>
+			</section>
+
+			<footer className="App-footer">
+				<button className="btn next-btn" onClick={handleText}>
+					{text}
+				</button>
+				<button className="btn ans-btn" onClick={handleClick}>
+					Next
+				</button>
+			</footer>
+		</div>
+	);
 }
 
 export default App;
